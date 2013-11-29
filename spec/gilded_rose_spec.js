81102a7998c25f0fa17fbf175efe7c1cb0bbb253
Thibaud Colas <thibaudcolas@gmail.com>
2013-11-29 09:04:08 +0100

    Add setUp / tearDown examples

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,11 +1,25 @@
 describe("Gilded Rose", function() {
+  var foo;
+
+  beforeEach(function() {
+    foo = 0;
+    foo += 1;
+  });
+
+  afterEach(function() {
+    foo = 0;
+  });
 
   it("should do something", function() {
 
   });
 
   it("is a syntax example", function() {
-     expect(true).toBe(true);
+    expect(true).toBe(true);
+  });
+
+  it("can have a negative case", function() {
+    expect(false).not.toBe(true);
   });
 
 });
