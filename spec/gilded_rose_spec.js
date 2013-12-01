e24bd8fcc11eb9b96e25409dcf9033df997423f7
Thibaud Colas <thibaudcolas@gmail.com>
2013-12-01 08:52:49 +0100

    Comment out test case samples

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,50 +1,50 @@
 describe("Gilded Rose", function() {
   var foo;
 
-  beforeEach(function() {
-    foo = 0;
-    foo += 1;
-  });
+  // beforeEach(function() {
+  //   foo = 0;
+  //   foo += 1;
+  // });
 
-  afterEach(function() {
-    foo = 0;
-  });
+  // afterEach(function() {
+  //   foo = 0;
+  // });
 
   it("should do something", function() {
 
   });
 
-  it("is a syntax example", function() {
-    expect(true).toBe(true);
-  });
+  // it("is a syntax example", function() {
+  //   expect(true).toBe(true);
+  // });
 
-  it("can have a negative case", function() {
-    expect(false).not.toBe(true);
-  });
+  // it("can have a negative case", function() {
+  //   expect(false).not.toBe(true);
+  // });
 
 
 
-  describe("Another spec", function() {
-    it("is just a function, so it can contain any code", function() {
-      var foo = 0;
-      foo += 1;
+  // describe("Another spec", function() {
+  //   it("is just a function, so it can contain any code", function() {
+  //     var foo = 0;
+  //     foo += 1;
 
-      expect(foo).toEqual(1);
-    });
+  //     expect(foo).toEqual(1);
+  //   });
 
-    it("The 'toBeLessThan' matcher is for mathematical comparisons", function() {
-      var pi = 3, e = 2;
+  //   it("The 'toBeLessThan' matcher is for mathematical comparisons", function() {
+  //     var pi = 3, e = 2;
 
-      expect(e).toBeLessThan(pi);
-      expect(pi).not.toBeLessThan(e);
-    });
+  //     expect(e).toBeLessThan(pi);
+  //     expect(pi).not.toBeLessThan(e);
+  //   });
 
-    it("The 'toBeGreaterThan' is for mathematical comparisons", function() {
-      var pi = 3, e = 2;
+  //   it("The 'toBeGreaterThan' is for mathematical comparisons", function() {
+  //     var pi = 3, e = 2;
 
-      expect(pi).toBeGreaterThan(e);
-      expect(e).not.toBeGreaterThan(pi);
-    });
-  });
+  //     expect(pi).toBeGreaterThan(e);
+  //     expect(e).not.toBeGreaterThan(pi);
+  //   });
+  // });
 
 });
