f7f296c676d2ff722d4c7124fc1f20ddf0d26da0
Thibaud Colas <thibaudcolas@gmail.com>
2013-12-19 03:06:35 +0100

    Add WIP 2 from Dojo

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -13,48 +13,50 @@ items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
 items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
+function update_sellin() {
+
+}
+
 function update_quality() {
   for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert' && items[i].name != 'Sulfuras, Hand of Ragnaros') {
       if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
+          items[i].quality--;
       }
-    } else {
+    } else if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
       if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
+        items[i].quality++;
         if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
           if (items[i].sell_in < 11) {
             if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+              items[i].quality++;
             }
           }
           if (items[i].sell_in < 6) {
             if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
+              items[i].quality++;
             }
           }
         }
       }
     }
     if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
+      items[i].sell_in--;
     }
     if (items[i].sell_in < 0) {
       if (items[i].name != 'Aged Brie') {
         if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
           if (items[i].quality > 0) {
             if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
+              items[i].quality--;
             }
           }
         } else {
-          items[i].quality = items[i].quality - items[i].quality
+          items[i].quality = 0;
         }
       } else {
         if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
+          items[i].quality++;
         }
       }
     }
