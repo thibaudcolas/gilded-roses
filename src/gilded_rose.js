20c16e9bfe3231d1c364f9b87655db2b299bf49d
Thibaud Colas <thibaudcolas@gmail.com>
2013-12-19 04:25:09 +0100

    Remove useless cruft

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -18,46 +18,46 @@ function update_quality() {
 }
 
 function update_item(item) {
-  if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+  if (item.name !== 'Aged Brie' && item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
     if (item.quality > 0) {
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
-        item.quality = item.quality - 1
+      if (item.name !== 'Sulfuras, Hand of Ragnaros') {
+        item.quality--;
       }
     }
   } else {
     if (item.quality < 50) {
-      item.quality = item.quality + 1
+      item.quality++;
       if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
         if (item.sell_in < 11) {
           if (item.quality < 50) {
-            item.quality = item.quality + 1
+            item.quality++;
           }
         }
         if (item.sell_in < 6) {
           if (item.quality < 50) {
-            item.quality = item.quality + 1
+            item.quality++;
           }
         }
       }
     }
   }
-  if (item.name != 'Sulfuras, Hand of Ragnaros') {
-    item.sell_in = item.sell_in - 1;
+  if (item.name !== 'Sulfuras, Hand of Ragnaros') {
+    item.sell_in--;
   }
   if (item.sell_in < 0) {
-    if (item.name != 'Aged Brie') {
-      if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+    if (item.name !== 'Aged Brie') {
+      if (item.name !== 'Backstage passes to a TAFKAL80ETC concert') {
         if (item.quality > 0) {
-          if (item.name != 'Sulfuras, Hand of Ragnaros') {
-            item.quality = item.quality - 1
+          if (item.name !== 'Sulfuras, Hand of Ragnaros') {
+            item.quality--;
           }
         }
       } else {
-        item.quality = item.quality - item.quality
+        item.quality = 0
       }
     } else {
       if (item.quality < 50) {
-        item.quality = item.quality + 1
+        item.quality++;
       }
     }
   }
