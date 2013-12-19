04ecece4140f98623975a6a9a78e907a0fda17e8
Thibaud Colas <thibaudcolas@gmail.com>
2013-12-19 04:33:54 +0100

    Removed ALL hard-coded values

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -17,33 +17,48 @@ function update_quality() {
   items.map(update_item);
 }
 
-var type = {
-  Aging: 'Aged Brie',
-  Legendary: 'Sulfuras, Hand of Ragnaros',
-  Event: 'Backstage passes to a TAFKAL80ETC concert'
+// Contains all values used to enforce business rules.
+var rule = {
+  type: {
+    Aging: 'Aged Brie',
+    Legendary: 'Sulfuras, Hand of Ragnaros',
+    Event: 'Backstage passes to a TAFKAL80ETC concert'
+  },
+  quality: {
+    max: 50,
+    min: 0
+  },
+  sell_in: {
+    close: 10,
+    closer: 5,
+    over: 0
+  }
 };
 
+/*
+ * Updates one item.
+ */
 function update_item(item) {
-  if (item.name === type.Legendary) {
+  if (item.name === rule.type.Legendary) {
     return;
   }
 
-  if (item.name !== type.Aging && item.name !== type.Event) {
-    if (item.quality > 0) {
+  if (item.name !== rule.type.Aging && item.name !== rule.type.Event) {
+    if (item.quality > rule.quality.min) {
       item.quality--;
     }
   }
   else {
-    if (item.quality < 50) {
+    if (item.quality < rule.quality.max) {
       item.quality++;
-      if (item.name == type.Event) {
-        if (item.sell_in < 11) {
-          if (item.quality < 50) {
+      if (item.name == rule.type.Event) {
+        if (item.sell_in <= rule.sell_in.close) {
+          if (item.quality < rule.quality.max) {
             item.quality++;
           }
         }
-        if (item.sell_in < 6) {
-          if (item.quality < 50) {
+        if (item.sell_in <= rule.sell_in.closer) {
+          if (item.quality < rule.quality.max) {
             item.quality++;
           }
         }
@@ -53,17 +68,17 @@ function update_item(item) {
 
   item.sell_in--;
 
-  if (item.sell_in < 0) {
-    if (item.name !== type.Aging) {
-      if (item.name !== type.Event) {
-        if (item.quality > 0) {
+  if (item.sell_in < rule.sell_in.over) {
+    if (item.name !== rule.type.Aging) {
+      if (item.name !== rule.type.Event) {
+        if (item.quality > rule.quality.min) {
           item.quality--;
         }
       } else {
-        item.quality = 0
+        item.quality = rule.quality.min;
       }
     } else {
-      if (item.quality < 50) {
+      if (item.quality < rule.quality.max) {
         item.quality++;
       }
     }
